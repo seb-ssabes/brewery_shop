7538df5674b8651cc9f45533b924a828f0ffa41e
Sebastian Olivares <olivares.heredia@gmail.com>
2024-06-25 14:52:32 +0200

    added current_cart method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_subscriber
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      cart = Cart.find_by(id: session[:cart_id])
+      if cart.present?
+        @current_cart ||= cart
+      else
+        session[:cart_id] = nil
+      end
+    end
+
+    if session[:cart_id].nil?
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
+  
+    @current_cart
+  end
 
   private
 
