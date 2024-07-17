1ecf5a5a97ba4f31ac18874fc32fc53299fbfee8
Sebastian Olivares <olivares.heredia@gmail.com>
2024-07-17 13:41:30 +0200

    stimulus order connected and target picked up

--- app/javascript/controllers/orders_controller.js
+++ app/javascript/controllers/orders_controller.js
@@ -15,5 +15,28 @@ export default class extends Controller {
 
   updateTotalPrice() {
   console.log("Shipping method chosen")
+
+  const shippingMethodId = event.target.value;
+  const orderId = this.element.dataset.orderId;
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+  fetch(`/orders/${orderId}/update_total_price`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": csrfToken
+    },
+    body: JSON.stringify({ order_id: orderId, shipping_method_id: shippingMethodId })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.error) {
+      console.error(data.error);
+    } else {
+      this.totalPriceTarget.innerText = `Total: ${data.total_price}`;
+    }
+  })
+  .catch(error => console.error('Error:', error));
+
   }
 }
