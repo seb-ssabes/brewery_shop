62dd8059fbb6096c045792b3fdced840a615f033
Sebastian Olivares <olivares.heredia@gmail.com>
2024-08-27 13:31:59 +0200

    removing temporarily user relations from carts to migrate to heroku

--- db/migrate/20240625103603_create_carts.rb
+++ db/migrate/20240625103603_create_carts.rb
@@ -1,7 +1,9 @@
 class CreateCarts < ActiveRecord::Migration[7.1]
   def change
     create_table :carts do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references null: false, foreign_key: true
+      # t.references :user, null: false, foreign_key: true USER NEEDS TO GO BACK AFTER DEPLOYMENT
+
 
       t.timestamps
     end
