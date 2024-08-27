6a887f1ada150a15bee6e65280720b7f7b448f3c
Sebastian Olivares <olivares.heredia@gmail.com>
2024-08-27 15:52:23 +0200

    adding users back to carts reference

--- db/migrate/20240625103603_create_carts.rb
+++ db/migrate/20240625103603_create_carts.rb
@@ -1,8 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.1]
   def change
     create_table :carts do |t|
-      t.references null: false, foreign_key: true
-      # t.references :user, null: false, foreign_key: true USER NEEDS TO GO BACK AFTER DEPLOYMENT
+      t.references :user, null: false, foreign_key: true
 
 
       t.timestamps
