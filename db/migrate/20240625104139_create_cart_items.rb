c3cb751c0745f44de8456e63920b92e9413eb45f
Sebastian Olivares <olivares.heredia@gmail.com>
2024-08-27 13:39:40 +0200

    removing temporarily product relations from carts_items to migrate to heroku

--- db/migrate/20240625104139_create_cart_items.rb
+++ db/migrate/20240625104139_create_cart_items.rb
@@ -2,7 +2,9 @@ class CreateCartItems < ActiveRecord::Migration[7.1]
   def change
     create_table :cart_items do |t|
       t.references :cart, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
+      t.references null: false, foreign_key: true
+      # t.references :product, null: false, foreign_key: true product NEEDS TO GO BACK AFTER DEPLOYMENT
+
       t.integer :quantity
 
       t.timestamps
