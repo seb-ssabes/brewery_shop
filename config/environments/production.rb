cf9a937c66727285c38355b60d17f68550b4114d
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-02 12:34:31 +0200

    refactored cloudinary environment/production again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,9 @@ Rails.application.configure do
   Cloudinary.config_from_url(ENV['CLOUDINARY_URL'])
 
   Cloudinary.config do |config|
+    config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+    config.api_key = ENV['CLOUDINARY_API_KEY']
+    config.api_secret = ENV['CLOUDINARY_API_SECRET']
     config.secure = true
   end
 end
