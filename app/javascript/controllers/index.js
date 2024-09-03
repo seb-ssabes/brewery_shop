4037d0559bcf60fdedca78c87af14a349cf01c52
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-03 11:22:25 +0200

    switched controller index to lazyLoad

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,14 +3,14 @@
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+lazyLoadControllersFrom("controllers", application)
 
 
-import OrdersController from "./orders_controller.js"
+// import OrdersController from "./orders_controller.js"
 
-application.register("orders", OrdersController)
+// application.register("orders", OrdersController)
