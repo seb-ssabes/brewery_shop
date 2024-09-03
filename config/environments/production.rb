9a12178efe151803cb67c7b8196238c7f36c6b1c
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-03 11:43:29 +0200

    edited production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.digest = true
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin 'bootstrap', to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
 
-# pin "controllers/orders_controller", to: "controllers/orders_controller.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
