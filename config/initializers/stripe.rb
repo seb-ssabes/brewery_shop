c604aba2673f3ccf7796de5f45755fbefb4d0444
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-09 12:34:52 +0200

    added stripe test keys to initializers

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key: ENV['STRIPE_SECRET_KEY'],
+    publishable_key: ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_TEST_SECRET_KEY'],
   }
 else
   Rails.configuration.stripe = {
