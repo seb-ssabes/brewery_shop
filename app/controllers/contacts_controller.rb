0d87df86f76a58721d63a6e85f90f589cbe558da
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-10 11:16:18 +0200

    contact confirmation message edited

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       # if Rails.env.production?
-        redirect_to new_contact_path, notice: 'We\'re sorry. This feature is under maintenance. Please contact us at thebrewingcompany@beer.com'
+        redirect_to new_contact_path, notice: 'Thank you for your message. We\'ll get back to you soon'
       # else
       #   ContactMailer.contact_email(@contact).deliver_now
       #   redirect_to new_contact_path, notice: 'Message sent successfully.'
